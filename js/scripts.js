78db0e3d0ac9fff1a0dc8ca61e9f9d318c66ae02
snowhero <31714174+snowhero@users.noreply.github.com>
2017-11-14 22:03:18 -0800

    Delete scripts.js

--- js/scripts.js
+++
@@ -1,3 +0,0 @@
-function myGoals()  {
-  console.log('I want to be able to apply, not just learn javascript. In addition, I ultimately want Ryeker to acknowledge me and become a part of my professional network.');
-}
